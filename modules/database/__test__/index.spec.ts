4d12e7f579e817e917ba37bc21c9fe8f314bbba9
LuoRain <luotianyi@luotianyi.me>
2026-05-17 16:26:33 +0800

    chore: cleanup napi-rs template

--- modules/database/__test__/index.spec.ts
+++ modules/database/__test__/index.spec.ts
@@ -1,14 +1,14 @@
-import os from 'node:os'
-import { resolve } from 'node:path'
+import os from "node:os";
+import { resolve } from "node:path";
 
-import test from 'ava'
+import test from "ava";
 
-import { Database } from '../index'
+import { Database } from "../index";
 
-const dbFile = resolve(os.tmpdir(), 'open-orpheus-tmp-db.db')
+const dbFile = resolve(os.tmpdir(), "open-orpheus-tmp-db.db");
 
-test('can create database', (t) => {
+test("can create database", (t) => {
   t.notThrows(() => {
-    new Database(dbFile)
-  })
-})
+    new Database(dbFile);
+  });
+});
